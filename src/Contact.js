fe3f06ed595a417157a659f75edea8a2fb60cce6
Akhil <akhilchandran030@gmail.com>
2026-01-25 15:27:08 +0530

    fixed all responsive issues

--- src/Contact.js
+++ src/Contact.js
@@ -10,9 +10,9 @@ const Contact = () => {
             <div className="contact" id="contact">
                 <h1 className="head_cont"><center>Contact Me</center></h1>
                 <div className="cont_details">
-                    <div><a href="mailto:akhilacs2001@gmail.com"> Email : akhilacs2001@gmail.com </a> </div>
-                    <div>Phone : +91 81378 23254</div>
-                    <div>Location : Trivandrum, Kerala</div>
+                    <div className="email"><a href="mailto:akhilacs2001@gmail.com"> Email : akhilacs2001@gmail.com </a> </div>
+                    <div className="phonenumber">Phone : +91 81378 23254</div>
+                    <div className="address">Location : Trivandrum, Kerala</div>
                 </div>
             </div>
         </>
