9dd7ba84a7994d5cca61b141e9fb0c756815806a
Akhil <akhilchandran030@gmail.com>
2026-01-25 10:45:39 +0530

    updated resume

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
     const instagram = 'https://www.instagram.com/akhix_';
     const github = 'https://github.com/akhilchandran030';
     const whatsappUrl = 'https://wa/+918137823254';
-    const resume = "https://drive.google.com/file/d/1kN1qIDbzRjDdA1AGmR_Kvb9Bb1T-0txV/view?usp=drive_link";
+    const resume = "https://drive.google.com/file/d/14-HYgU2YD4luHresIQIdPwTqGjK9lsKV/view?usp=drive_link";
     return (
         <>
             <div className="maindiv" id='home'>
